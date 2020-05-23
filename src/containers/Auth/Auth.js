52e256284a4a3c53fc4a522418d82ce599219056
codesmitty <varcodesmitty@gmail.com>
2020-05-22 21:21:16 -0400

    updates

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,13 @@
 import React, { Component }from 'react';
+import { connect } from 'react-redux';
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button'
 import classes from './Auth.module.css';
+import * as actions from '../../store/actions/index';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import {Redirect} from 'react-router-dom';
+import { updateObject, checkValidity} from '../../shared/utility';
+
 class Auth extends Component {
     state={
         controls:{
@@ -33,8 +39,41 @@ class Auth extends Component {
                 valid: false,
                 touched: false
             }
+        }, 
+        isSignup:true
+    }
+
+
+
+    componentDidMount() {
+        if(!this.props.buildingBurger && this.props.authRedirectPath !== "/"){
+            this.props.onSetAuthRedirectPath
         }
     }
+
+ 
+
+    inputChangedHandler = (event, controlName ) => {
+        const updatedControls = updateObject(this.state.controls, {
+            [controlName]:updateObject(this.state.controls[controlName], {
+                value:event.target.value,
+                valid:checkValidity(event.target.value, this.state.controls[controlName].validation),
+                touched:true
+            })
+        })
+        this.setState({controls:updatedControls});
+    }
+
+    submitHandler = (event ) =>{
+        event.preventDefault();
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup)
+    }
+
+    switchAuthModeHandler = () => {
+        this.setState(prevState =>{
+            return{isSignup: !prevState.isSignup}
+        })
+    };
 
 
     render(){
@@ -46,7 +85,9 @@ class Auth extends Component {
             });
         }
 
-        const form = formElementsArray.map(formElement =>(
+        
+
+        let form = formElementsArray.map(formElement =>(
             <Input 
                         key={formElement.id}
                         elementType={formElement.config.elementType}
@@ -58,15 +99,55 @@ class Auth extends Component {
                         changed={(event) => this.inputChangedHandler(event, formElement.id)} />
                         
         ))
+
+        if(this.props.loading){
+            form = <Spinner />
+        }
+
+        let errorMessage = null;
+
+            if(this.props.error){
+                errorMessage = (
+                    <p>{this.props.error.message}</p>
+                )
+            }
+
+        let  authRedirect = null
+            if(this.props.isAuthenticated){
+                authRedirect = <Redirect to={this.props.authRedirectPath} />
+            }
         return(
             <div className={classes.Auth}>
-                <form>
+                {authRedirect}
+                {errorMessage}
+                <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">Submit</Button>
                 </form>
+                    <Button 
+                    clicked={this.switchAuthModeHandler}
+                    btnType="Danger"
+                    >SWITCH TO {this.state.isSignup ? 'Sign In' : "Signup"} </Button>
             </div>
         );
     }
 }
 
-export default Auth;
+const mapStateToProps = state => {
+    return{
+        loading:state.auth.loading, 
+        error:state.auth.error,
+        isAuthenticated: state.auth.token !== null,
+        buildingBurger:state.burgerBuilder.building,
+        authRedirectPath:state.auth.authRedirectPath
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return{
+        onAuth:(email, password, isSignup) => dispatch(actions.auth(email, password, isSignup)),
+        onSetAuthRediretPath: () => dispatch(actions.setAuthRedirectPath('/'))
+    }
+}
+
+export default connect( mapStateToProps, mapDispatchToProps)(Auth);
--- src/utility.js
+++ src/utility.js
@@ -1,3 +1,4 @@
+
 export const updateObject = (oldObject, updatedProperties) => {
     return {
         ...oldObject,
